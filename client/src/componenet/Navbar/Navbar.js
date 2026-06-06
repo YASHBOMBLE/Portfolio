a535a3c5d5917297d43ee7cf51963a3127d64a23
Yash Bomble <113371406+YASHBOMBLE@users.noreply.github.com>
2026-06-06 14:45:06 +0530

    navbar change UI

--- client/src/componenet/Navbar/Navbar.js
+++ client/src/componenet/Navbar/Navbar.js
@@ -15,41 +15,20 @@ function Navbar() {
   }, [theam]);
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top height">
-        <div class="container-fluid">
-          <a href="#" className='logo'>Yash <span> Bomble</span></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 navbar item">
-              <li class="">
-                <a class="" aria-current="page" href="#home">Home</a>
-              </li>
-              <li class="">
-                <a class="" href="#about">About</a>
-              </li>
-              <li class="">
-                <a class="" href='#skills'>Skills</a>
-              </li>
-              <li class="">
-                <a class="" href="#services">Project</a>
-              </li>
-              <li>
-                <a className='' href='#certificate'>Certificate</a>
-              </li>
-              <li>
-                <a className='' href='#education'>Education</a>
-              </li>
-              <li class="">
-                <a class="" href="#contact">Contact</a>
-              </li>
-              <div class="bx bx-moon" id="darkmode" onClick={toggleTheme}></div>
-            </ul>
+      <div id="cur-dot"></div>
+<div id="cur-ring"></div>
 
-          </div>
-        </div>
-      </nav>
+<!-- NAVBAR -->
+<nav id="navbar">
+  <div class="nav-logo" onclick="showPage('home')">AM</div>
+  <ul class="nav-links">
+    <li><a onclick="showPage('home')" data-page="home">Home</a></li>
+    <li><a onclick="showPage('about')" data-page="about">About</a></li>
+    <li><a onclick="showPage('projects')" data-page="projects">Projects</a></li>
+    <li><a onclick="showPage('skills')" data-page="skills">Skills</a></li>
+    <li><a onclick="showPage('contact')" data-page="contact">Contact</a></li>
+  </ul>
+</nav>
 
     </div>
   )
